5988314ce2fa8efe9707536e8bcfa58444a4e550
jemunozg <jemunozg@unal.edu.co>
2024-04-01 09:47:29 -0500

    "Actualizando Routes en Web.php"

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::getprefix('productos')->group(funtion(){
+    Route::get('/',[ProductosController::class,'index'])->name('productos.index');
+    Route::get('crear',[ProductosController::class,'create'])->name('productos.create');
+    Route::post('store',[ProductosController::class,'store'])->name('productos.store');
+    Route::get('editar/{id}',[ProductosController::class,'edit'])->name('productos.edit');
+    Route::put('actualizar/{id}',[ProductosController::class,'update'])->name('productos.update');
+    Route::delete('eliminar/{id}',[ProductosController::class,'destroy'])->name('productos.destroy');
+
+    Route::resource('productos','\App\Http\Controllers\ProductosController') //Pasamos el nombre del controlador completo
 });
