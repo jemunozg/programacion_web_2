4e8a6604f1a8d445a7937041b7c2d8336d0065c4
jemunozg <jemunozg@unal.edu.co>
2024-04-01 09:34:12 -0500

    "Crea el Controlado Producto"

--- app/Models/productos.php
+++ app/Models/productos.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class productos extends Model
 {
     use HasFactory;
+    protected $table = 'productos'; // se especifica la tabla que va a utilizar.
+    protected $primaryKey='producto_id';//se indica el nombre de la clave primaria.
 }
